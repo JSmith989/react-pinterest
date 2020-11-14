6f72cd0e9984a49d4103219547ae3311e1ea94e3
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-11-14 10:58:21 -0600

    routing some more routes

--- src/App/App.js
+++ src/App/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom';
 import firebase from 'firebase/app';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from '../helpers/Routes';
 import fbConnection from '../helpers/data/connection';
 import './App.scss';
 import MyNavbar from '../components/MyNavbar';
-import Home from '../views/Home';
-import BoardForm from '../views/BoardForm';
-import Boards from '../views/Boards';
-import PinDetails from '../views/PinDetails';
-import PinForm from '../views/PinForm';
-import Pins from '../views/Pins';
-import SingleBoard from '../views/SingleBoard';
-import NotFound from '../views/NotFound';
 
 fbConnection();
 
@@ -44,18 +32,9 @@ class App extends React.Component {
 
     return (
       <div className='App'>
-        <MyNavbar authed={authed} />
         <Router>
-          <Switch>
-            <Route exact path='/' component={() => <Home authed={authed}/>} />
-            <Route exact path='/board-form' component={() => <BoardForm authed={authed}/>} />
-            <Route exact path='/boards' component={() => <Boards authed={authed}/>} />
-            <Route exact path='/pin-details' component={() => <PinDetails authed={authed}/>} />
-            <Route exact path='/pin-form' component={() => <PinForm authed={authed}/>} />
-            <Route exact path='/pins' component={() => <Pins authed={authed}/>} />
-            <Route exact path='/single-board' component={() => <SingleBoard authed={authed}/>} />
-            <Route component={NotFound}/>
-         </Switch>
+        <MyNavbar authed={authed} />
+        <Routes authed={authed} />
         </Router>
       </div>
     );
