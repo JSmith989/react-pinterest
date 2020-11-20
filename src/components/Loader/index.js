45ff330e5df412b35efa2a1300a5302e5426acb3
Jordan <67443077+JSmith989@users.noreply.github.com>
2020-11-19 23:56:31 -0600

    bed time

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,15 +1,15 @@
 const Loader = () => (
   <div className='mt-5'>
-    <div className='spinner-grow text-primary' role='status'>
+    <div className='spinner-grow text-dark' style={{ width: '10rem', height: '10rem' }} role='status'>
       <span className='sr-only'>Loading...</span>
     </div>
-    <div className='spinner-grow text-secondary' role='status'>
+    <div className='spinner-grow text-info' style={{ width: '10rem', height: '10rem' }} role='status'>
       <span className='sr-only'>Loading...</span>
     </div>
-    <div className='spinner-grow text-success' role='status'>
+    <div className='spinner-grow text-secondary' style={{ width: '10rem', height: '10rem' }} role='status'>
       <span className='sr-only'>Loading...</span>
     </div>
-    <div className='spinner-grow text-danger' role='status'>
+    <div className='spinner-grow text-warning' style={{ width: '10rem', height: '10rem' }} role='status'>
       <span className='sr-only'>Loading...</span>
     </div>
     <div>Loading...</div>
--- src/views/Pins.js
+++ src/views/Pins.js
@@ -1,9 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { getAllUserPins } from '../helpers/data/pinData';
+import PinsCard from '../components/Cards/PinsCard';
+import Loader from '../components/Loader';
+import getUid from '../helpers/data/authData';
+// import PinForm from '../components/Forms/PinForm';
+// import AppModal from '../components/AppModal';
 
-export default function Pins(props) {
-  return (
-    <div>
-      <h1>PINS</h1>
-    </div>
-  );
+export default class Pins extends Component {
+  state = {
+    pins: [],
+    loading: true,
+  }
+
+  componentDidMount() {
+    this.getPins();
+  }
+
+  getPins = () => {
+    const currentUserId = getUid();
+    getAllUserPins(currentUserId).then((response) => {
+      this.setState({
+        pins: response,
+      }, this.setLoading);
+    });
+  }
+
+  setLoading = () => {
+    this.timer = setInterval(() => {
+      this.setState({ loading: false });
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  render() {
+    const { pins, loading } = this.state;
+    console.warn(this.state.pins);
+    const showPins = () => {
+      pins.map((pin) => <PinsCard key={pin.firebaseKey} pin={pin} />);
+    };
+    return (
+      <>
+      { loading ? (
+        <Loader />
+      ) : (
+        <>
+        {/* <AppModal title={'Create Pin'} buttonLabel={'Create Pin'}>
+          <PinForm pin={pins} onUpdate={this.getPins}/>
+            </AppModal> */}
+          <h2>Here are all of your pins</h2>
+          <div className='d-flex flex-wrap container'>{showPins()}</div>
+        </>
+      )}
+      </>
+    );
+  }
 }
